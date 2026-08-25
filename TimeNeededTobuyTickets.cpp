f446afe662e163f39de68d741c43437fcbdacbe9
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-08-25 15:26:01 +0530

    Optimized my version of code added

--- TimeNeededTobuyTickets.cpp
+++ TimeNeededTobuyTickets.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int TimeRequired(vector<int> &arr, int k){
+/*int TimeRequired(vector<int> &arr, int k){
     queue<int> q;
     for(int i =0; i<arr.size(); i++)
         q.push(i);
@@ -13,11 +13,27 @@ int TimeRequired(vector<int> &arr, int k){
     q.pop();
     time++;
     }
-return time;
+    return time;
+}
+    */
+
+    //Optimal one
+    int requiredTime(vector<int>&tickets, int k){
+        int time = tickets[k];
+        for(int i =0; i<tickets.size(); i++){
+            if(i<k){
+                time+=min(tickets[i],tickets[k]);
+            }
+            else if(i>k){
+                time+=min(tickets[i], tickets[k]-1);
+            }
+        }
+        return time;
     }
+
     int main(){
         vector<int> arr = {1,6,2,3,7};
-        cout<<TimeRequired(arr,2);
+        cout<<requiredTime(arr,2);
         return 0;
     }
 
