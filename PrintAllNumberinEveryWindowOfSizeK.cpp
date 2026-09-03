082bcac6476c3a7324a46a485465f60171f7dcfb
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-09-03 20:01:23 +0530

    Chamged

--- PrintAllNumberinEveryWindowOfSizeK.cpp
+++ PrintAllNumberinEveryWindowOfSizeK.cpp
@@ -7,19 +7,19 @@ void display(queue<int> q){
     }
     cout<<endl;
 }
-void Print(vector<int> &arr, int k){
+void Print(int arr[], int k,int n){
     queue<int> q;
     for(int i =0; i<k-1; i++){
         q.push(arr[i]);
     }
-    for(int i = k-1; i<arr.size(); i++){
+    for(int i = k-1; i<n; i++){
         q.push(arr[i]);
         display(q);
         q.pop();
     }
 }
 int main(){
-    vector<int> arr = {1,2,3,4,5,6,7,8};
-    Print(arr,3);
+    int arr[] = {1,2,3,4,5,6,7,8};
+    Print(arr,3,8);
     return 0;
 }
