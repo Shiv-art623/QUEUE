152c2c279c56dffd90249270f3f19e4208efacb8
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-09-04 20:17:43 +0530

    Optimal sol added

--- 1stNegativeIntegerINeveryWindowK.cpp
+++ 1stNegativeIntegerINeveryWindowK.cpp
@@ -23,11 +23,33 @@ vector<int> FirstNegative(int arr[], int k, int n){
         q.pop();
     }
 return ans;
-    
 }
+//BUt in this method the time complexity was O(nk), so it is not a optimal one..
+
+//OPtimal Solution ---> O(n)
+vector<int> FirstInteger(int arr[], int k, int n){
+    queue<int> q;
+    for(int i =0; i<k-1; i++){
+        if(arr[i]<0)
+        q.push(i);
+    }
+    vector<int> ans;
+    for(int i = k-1; i<n; i++){
+        if(arr[i]<0) q.push(i);
+        if(q.empty()) ans.push_back(0);
+        else{
+            if(q.front()<=i-k) q.pop();
+            if(q.empty()) ans.push_back(0);
+            else ans.push_back(arr[q.front()]);
+        }
+    }
+    return ans;
+}
+    
+
 int main(){
     int arr[] = {2,-3,-4,-2,7,8,9,-10};
-    vector<int> ans = FirstNegative(arr,3,8);
+    vector<int> ans = FirstInteger(arr,3,8);//FirstNegative(arr,3,8);
    for(auto i : ans){
     cout<<i<<" ";
    }
