bc079e0b101d255b976098e808b21efb99d72dc8
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-09-05 14:55:16 +0530

    First non repeating char

---
+++ FirstNonRepeatingCharacter.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string firstNonRepeating(string &s) {
+        // code here
+        string B = "";
+        vector<int> repeat(26,0);
+        queue<char> q;
+        for(int i =0; i<s.size(); i++){
+            if(repeat[s[i]-'a']>=1){
+                repeat[s[i]-'a']++;
+                while(!q.empty() && repeat[q.front()-'a']>1) q.pop();
+            
+            if(q.empty()) B+='#';
+            else B+=q.front();
+            }
+            else{
+                repeat[s[i]-'a']++;
+                q.push(s[i]);
+                
+                while(repeat[q.front()-'a']>1) q.pop();
+                
+                B+=q.front();
+            }
+            
+            }
+            return B;
+        }
+        int main(){
+            string s = "abcacdbd";
+            cout<<firstNonRepeating(s);
+            return 0;
+        }
