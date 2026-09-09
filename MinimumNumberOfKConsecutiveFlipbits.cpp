402b843e92be2693becc5779de6cb9b7b8d99aef
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-09-09 15:21:02 +0530

    Optimal solution

---
+++ MinimumNumberOfKConsecutiveFlipbits.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+  int minKBitFlips(vector<int>& nums, int k) {
+        queue<int> q;
+        int flips = 0;
+        for(int i =0; i<nums.size(); i++){
+            if(!q.empty() && q.front()<i) q.pop();
+            if(q.size()%2 == nums[i]){
+                if(i+k-1>= nums.size()) return -1;
+
+                q.push(i+k-1);
+                flips++;
+            }
+        }
+        return flips;
+    }
+    int main(){
+        vector<int> arr = {0,0,1,0,0,1,1,0,1,0};
+        cout<<minKBitFlips(arr,4);
+        return 0;
+    }
