69cfb0d71d875489b58fef9ad9e0a656819c725a
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-09-09 15:31:42 +0530

    Brute force method added

--- MinimumNumberOfKConsecutiveFlipbits.cpp
+++ MinimumNumberOfKConsecutiveFlipbits.cpp
@@ -1,5 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//Brute Force-- O(n*k)
+
+int minkbitFlips(vector<int>&nums, int k){
+    int flip = 0;
+    for(int i =0; i<nums.size(); i++){
+        if(nums[i] == 0){
+            if(i+k > nums.size()) return -1;
+            for(int j =i; j<i+k; j++){
+                nums[j]= !nums[j];
+            }
+              flip++;
+        }
+    }
+    return flip;
+}
+//Optimal  O(n)
   int minKBitFlips(vector<int>& nums, int k) {
         queue<int> q;
         int flips = 0;
@@ -16,6 +33,7 @@ using namespace std;
     }
     int main(){
         vector<int> arr = {0,0,1,0,0,1,1,0,1,0};
-        cout<<minKBitFlips(arr,4);
+        cout<<minkbitFlips(arr,4);
+        //minKBitFlips(arr,4);
         return 0;
     }
